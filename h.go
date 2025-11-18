041400988e42f218b9cd3921c4b15ab220d39a81
jawadh-html <jh366681@gmail.com>
2025-11-18 00:43:18 -0800

    maps

--- h.go
+++ h.go
@@ -4,24 +4,83 @@ import "fmt"
 
 func main() {
 
-	var choice int
 
-	fmt.Print("============ USER DIRECTORY ============")
+    users := make(map[string]string)
 
-	fmt.Print("\n1. Add new users. ")
+    for {
 
-	fmt.Print("\n2. Update a user Email. ")
+        var choice int
 
-	fmt.Print("\n3. Delete a user. ")
+        fmt.Print("\n============ USER DIRECTORY ============")
+        fmt.Print("\n1. Add new users.")
+        fmt.Print("\n2. Update a user Email.")
+        fmt.Print("\n3. Delete a user.")
+        fmt.Print("\n4. Print all users.")
+        fmt.Print("\n5. Exit")
+        fmt.Print("\nChoose option: ")
+        fmt.Scan(&choice)
 
-	fmt.Print("\n4. Print all users. ")
+        if choice == 5 {
 
-	fmt.Print("\n5. Choose option: ")
-	fmt.Scan(&choice)
+            fmt.Print("bye ")
 
-	if choice == 5 {
-		fmt.Print("BYE ")
+            break
 
-	}
+        }
 
+        switch choice {
+			
+        case 1:
+
+            var name, email string
+
+            fmt.Print("Enter user name: ")
+            fmt.Scan(&name)
+
+            fmt.Print("Enter user email: ")
+            fmt.Scan(&email)
+
+            users[name] = email
+
+            fmt.Printf("User %s added successfully!\n", name)
+
+        case 2:
+            var name, newemail string
+
+            fmt.Print("Enter Users name: ")
+            fmt.Scan(&name)
+
+            fmt.Print("Enter new email: ")
+            fmt.Scan(&newemail)
+
+            users[name] = newemail
+
+            fmt.Printf("User %s updated successfully!\n", name) 
+
+        case 3:
+            var name string
+
+            fmt.Print("Enter users name: ")
+            fmt.Scan(&name)
+
+            delete(users, name)
+
+            fmt.Printf("User %s deleted successfully!\n", name) 
+
+        case 4:
+
+            fmt.Println("\n====== All USERS =====")
+
+            for name, email := range users {
+
+                fmt.Printf("User name: %s | Email: %s\n", name, email)
+
+            }
+
+        default: 
+
+            fmt.Print("Invalid choice please select between 1-5 ")
+
+        }
+    }
 }
